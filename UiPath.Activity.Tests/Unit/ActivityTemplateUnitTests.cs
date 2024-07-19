4409e79e4ddb1fd2a77c11f27e09a4f04ae04ddc
ravelaso <ravelaso@outlook.com>
2024-07-19 08:43:33 +0200

    init

---
+++ UiPath.Activity.Tests/Unit/ActivityTemplateUnitTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace UiPath.Activity.Tests.Unit
+{
+    public class ActivityTemplateUnitTests
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(0, 0);
+        }
+    }
+}
---
+++ UiPath.Activity.Tests/Workflow/ActivityTemplateWorkflowTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace UiPath.Activity.Tests.Tests.Workflow
+{
+    public class ActivityTemplateWorkflowTests
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(0, 0);
+        }
+
+    }
+}
